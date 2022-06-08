cf42897eed53f410f583715436a100d580c1b644
ovidiorava <osaccani@rava.com>
2022-06-07 22:36:59 -0300

    dependencias y pantalla inicial login

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { StackNavigator } from "./src/navigation/StackNavigator";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 
 const App = () => {
-	return <SafeAreaView>
-    <Text>Hola mundo</Text>
-  </SafeAreaView>;
+	return (
+		<NavigationContainer>
+			<StackNavigator />
+		</NavigationContainer>
+	);
 };
 
 const styles = StyleSheet.create({});
---
+++ src/components/InputLogin.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+interface Props {
+	title: string;
+	isPassword?: boolean;
+	value: string;
+	setValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const InputLogin = ({
+	title,
+	isPassword = false,
+	value,
+	setValue,
+}: Props) => {
+	const [showPassword, setShowPassword] = useState(isPassword);
+
+	return (
+		<View style={styles.containerInput}>
+			<Text style={styles.fontColor}>{title}</Text>
+			<View style={styles.sectionStyle}>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => {
+						setValue(value);
+					}}
+					value={value}
+					secureTextEntry={showPassword}
+				/>
+				{isPassword ? (
+					<Icon
+						name={showPassword ? "eye-outline" : "eye-off-outline"}
+						size={25}
+						style={styles.iconStyle}
+						onPress={() => setShowPassword(!showPassword)}
+					/>
+				) : null}
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	containerInput: {
+		width: "100%",
+	},
+	input: {
+		flex: 1,
+		marginLeft: 4,
+	},
+	fontColor: {
+		color: "#00386E",
+	},
+	sectionStyle: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#43434403",
+		height: 40,
+		borderWidth: 1,
+		borderRadius: 10,
+		marginTop: 4,
+		borderColor: "#00386E7D",
+	},
+	iconStyle: {
+		alignItems: "center",
+		marginRight: 8,
+	},
+});
