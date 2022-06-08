5a3ef8cf36bccf53a5e951d1282647bc715ca892
ovidiorava <osaccani@rava.com>
2022-06-07 21:45:17 -0300

    estructura de carpetas y primer hola mundo

---
+++ src/api/peliculaBd.tsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+
+const peliculaBd = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/movie',
+    params: {
+        api_key: '1e49446da98a104466d148a6578fc8f2',
+        language: 'es-ES'
+    }
+});
+
+
+export default peliculaBd;
+
+
